5e0f1f822fba2bc0f29e20b7f1ed752a6e4eafc1
Alex Ripoll <alexripollpiera@gmail.com>
2022-12-04 21:02:31 +0100

    updating keymaps

--- lua/alexrp/core/keymaps.lua
+++ lua/alexrp/core/keymaps.lua
@@ -1,6 +1,5 @@
 -- set leader key to space
 vim.g.mapleader = " "
-
 local keymap = vim.keymap -- for conciseness
 
 ---------------------
@@ -31,6 +30,28 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- Better window navigation
+keymap.set("n", "<C-h>", "<C-w>h")
+keymap.set("n", "<C-j>", "<C-w>j")
+keymap.set("n", "<C-k>", "<C-w>k")
+keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Navigate buffers
+keymap.set("n", "<S-l>", ":bnext<CR>")
+keymap.set("n", "<S-h>", ":bprevious<CR>")
+
+-- Clear highlights
+keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>")
+
+-- Close buffers
+keymap.set("n", "<S-q>", "<cmd>bdelete!<CR>")
+
+-- Better paste
+keymap.set("v", "p", '"_dP')
+
+-- Stay in indent mode
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
 
 ----------------------
 -- Plugin Keybinds
@@ -48,3 +69,10 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- Git
+keymap.set("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>")
+
+-- Comment
+keymap.set("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>")
+keymap.set("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
