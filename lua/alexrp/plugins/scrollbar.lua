f1d32945ed42c38efe46284a068eef31d2cd0b1c
Alex Ripoll <alexripollpiera@gmail.com>
2022-12-13 20:26:36 +0100

    adding protected call to scrollbar

--- lua/alexrp/plugins/scrollbar.lua
+++ lua/alexrp/plugins/scrollbar.lua
@@ -1,4 +1,9 @@
-require("scrollbar").setup({
+local status_ok, scrollbar = pcall(require, "scrollbar")
+if not status_ok then
+	return
+end
+
+scrollbar.setup({
 	show = true,
 	show_in_active_only = false,
 	set_highlights = true,
