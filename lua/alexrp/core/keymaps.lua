88a98ef763f5f0eef7adf946f36b4842e695cd9f
Alex Ripoll <alexripollpiera@gmail.com>
2022-12-21 21:54:59 +0100

    updating keymaps

--- lua/alexrp/core/keymaps.lua
+++ lua/alexrp/core/keymaps.lua
@@ -53,6 +53,13 @@ keymap.set("v", "p", '"_dP')
 keymap.set("v", "<", "<gv")
 keymap.set("v", ">", ">gv")
 
+-- keymap.set("v", "<C-j>", ":m '>+1<CR>'gv=gv")
+-- keymap.set("v", "<C-k>", ":m '<-2<CR>'gv=gv")
+
+-- Move text up and down
+keymap.set("v", "<A-j>", ":m .+1<CR>==")
+keymap.set("v", "<A-k>", ":m .-2<CR>==")
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
@@ -66,6 +73,7 @@ keymap.set("n", "<C-n>", ":NvimTreeFindFile<CR>")
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>") -- find files tracked by git
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
