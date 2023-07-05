230bb11e273db4c3bd5df003aab3444b008158d1
Alex Ripoll <alexripollpiera@gmail.com>
2023-07-05 13:08:34 +0200

    cleaning init.lua

--- init.lua
+++ init.lua
@@ -38,8 +38,10 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
+
+require 'config.options'
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -259,52 +261,14 @@ require('lazy').setup({
   { import = 'plugins.lsp' },
 }, {})
 
-require 'config.options'
 require 'config.keymaps'
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
--- Set highlight on search
-vim.o.hlsearch = false
 
--- Make line numbers default
-vim.wo.number = true
 
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeout = true
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
--- [[ Basic Keymaps ]]
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -463,7 +427,7 @@ local servers = {
   helm_ls = {},
   jsonls = {},
   yamlls = {},
-  tamplo = {},
+  taplo = {},
   marksman = {},
   lua_ls = {
     Lua = {
@@ -502,7 +466,7 @@ mason_lspconfig.setup_handlers {
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
+-- luasnip.config.setup {}
 
 cmp.setup {
   snippet = {
