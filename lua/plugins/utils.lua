98669f9589a0d7d46b54541ba8aef28d606cca20
Alex Ripoll <alexripollpiera@gmail.com>
2023-11-01 23:23:38 +0100

    adding whichkey config

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -3,6 +3,17 @@ return {
   {
     'folke/which-key.nvim',
     opts = {},
+    config = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>f"] = { name = "+find" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>h"] = { name = "+harpoon" },
+        ["<leader>t"] = { name = "+terminal" },
+        ["<leader>w"] = { name = "+workspace" },
+      })
+    end
   },
 
   -- Persist session
