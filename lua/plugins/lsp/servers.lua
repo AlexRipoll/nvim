df52b5a34f280e43f63a4d666c939e1a77342394
Alex Ripoll <alexripollpiera@gmail.com>
2023-11-01 23:24:33 +0100

    adding rust-analyzer server configuration

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -4,7 +4,31 @@ M.servers = {
   -- clangd = {},
   gopls = {},
   -- pyright = {},
-  rust_analyzer = {},
+  rust_analyzer = {
+    cargo = {
+      allFeatures = true,
+      loadOutDirsFromCheck = true,
+      runBuildScripts = true,
+    },
+    -- Add clippy lints for Rust.
+    checkOnSave = {
+      allFeatures = true,
+      command = "clippy",
+      extraArgs = { "--no-deps" },
+    },
+    files = {
+      watcher = "server",
+    },
+    procMacro = {
+      enable = true,
+      ignored = {
+        ["async-trait"] = { "async_trait" },
+        ["napi-derive"] = { "napi" },
+        ["async-recursion"] = { "async_recursion" },
+      },
+    },
+
+  },
   solang = {},
   tsserver = {},
   docker_compose_language_service = {},
