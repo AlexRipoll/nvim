f6fb52bae6676f76019fa3798a1e980bc0554c15
Alex Ripoll <alexripollpiera@gmail.com>
2023-12-02 13:55:57 +0100

    updating neo-tree icons

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -70,11 +70,11 @@ return {
       vim.fn.sign_define("DiagnosticSignError",
         { text = "✖ ", texthl = "DiagnosticSignError" })
       vim.fn.sign_define("DiagnosticSignWarn",
-        { text = " ", texthl = "DiagnosticSignWarn" })
+        { text = " ", texthl = "DiagnosticSignWarn" })
       vim.fn.sign_define("DiagnosticSignInfo",
-        { text = " ", texthl = "DiagnosticSignInfo" })
+        { text = "󰙎", texthl = "DiagnosticSignInfo" })
       vim.fn.sign_define("DiagnosticSignHint",
-        { text = "", texthl = "DiagnosticSignHint" })
+        { text = "󰮯", texthl = "DiagnosticSignHint" })
       -- NOTE: this is changed from v1.x, which used the old style of highlight groups
       -- in the form "LspDiagnosticsSignWarning"
 
@@ -132,8 +132,8 @@ return {
           git_status = {
             symbols = {
               -- Change type
-              added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
-              modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
+              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
               deleted   = "✖", -- this can only be used in the git_status source
               renamed   = "", -- this can only be used in the git_status source
               -- Status type
