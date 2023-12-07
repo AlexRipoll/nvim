12e750373e6867e10c52dfa732a648e8bc02b2c6
Alex Ripoll <alexripollpiera@gmail.com>
2023-12-07 14:55:51 +0100

    removing unneeded keymaps beacause of vim-tmux navigator plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,12 +27,6 @@ map('n', '<leader>n', ':Neotree reveal<CR>', { desc = 'Reveal file in tree explo
 map('n', '<leader>sx', ':close<CR>', { desc = 'Close current split window' })
 
 
--- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-
 -- Resize window using <ctrl> arrow keys
 map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
